e6e3ece32ce5ffbf3043a88426a22c64c847a7bb
Omar Nada <ndomar.14@gmail.com>
2017-05-17 20:23:45 +0200

    Added concatent file sh

---
+++ concatenate_csv_files.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+declare -a machines=("ssh_handle_1" "ssh_handle_2" "ssh_handle3" "ssh_handle4" "ssh_handle5" "ssh_handle_6" "ssh_handle_7" "ssh_handle8" "ssh_handle9" "ssh_handle10")
+out_file_name="final_results.csv"
+file_name="metrics-results.csv"
+for i in "${machines[@]}"
+do
+	ssh $i
+	sciprec_results
+	tail $file_name >> $out_file_name
+done
+
